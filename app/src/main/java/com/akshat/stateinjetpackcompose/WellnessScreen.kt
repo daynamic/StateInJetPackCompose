815dc0dbeac11928507bc11f3d09eb924a02f8dc
Akshat <akshat@akshats-mbp.home>
2022-11-24 11:30:31 -0500

    Initial commit State in JetPack Component

---
+++ app/src/main/java/com/akshat/stateinjetpackcompose/WellnessScreen.kt
@@ -0,0 +1,9 @@
+package com.akshat.stateinjetpackcompose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
